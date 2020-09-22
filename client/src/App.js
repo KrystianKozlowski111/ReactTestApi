9c43affbb0aac5452671f529c3b82aff053c501b
KrystianKozlowski <krystiankozlowski111@gmail.com>
2020-09-22 12:33:13 +0200

    client added

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Head from './components/Head';
+function App() {
+  return (
+    <div className="App">
+      <Head />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Head.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Heading = styled.h1`
+  color: black;
+  text-align: left;
+`;
+const Head = () => {
+  return (
+    <div className="head">
+      <Heading>Red</Heading>
+      <p>dsadsa</p>
+    </div>
+  );
+};
+//c
+export default Head;
