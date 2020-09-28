921b2c8bbba9f4f89fd8b4f060b804db9968e2c7
KrystianKozlowski <krystiankozlowski111@gmail.com>
2020-09-28 15:57:30 +0200

    Apollo provider added to boxstory

---
+++ client/src/stories/Box.stories.js
@@ -0,0 +1,25 @@
+import { storiesOf } from '@storybook/react';
+import React from 'react';
+
+import Box from '../components/box';
+
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { ApolloProvider as ApolloProvider2 } from '@apollo/client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000',
+});
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+storiesOf('Box', module).add('Box', () => (
+  <ApolloProvider2 client={client}>
+    <ApolloProvider client={client}>
+      <Box />
+    </ApolloProvider>
+  </ApolloProvider2>
+));
--- client/src/stories/Flex.stories.js
+++
@@ -1,6 +0,0 @@
-import { storiesOf } from '@storybook/react';
-import React from 'react';
-
-import Flex from '../components/flex';
-
-storiesOf('Flex', module).add('Flex', () => <Flex />);
