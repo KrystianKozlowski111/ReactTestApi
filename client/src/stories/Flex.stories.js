74334e12b0c8fe8bfba7b19dbb8f560bb23066aa
KrystianKozlowski <krystiankozlowski111@gmail.com>
2020-10-05 12:55:18 +0200

    flex.stories.js not working

--- client/src/stories/Flex.stories.js
+++ client/src/stories/Flex.stories.js
@@ -1,5 +1,27 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Flex from '../components/Flex';
+import { configure, addDecorator } from '@storybook/react';
+import apolloStorybookDecorator from 'apollo-storybook-react';
+const typeDefs = `
+type Color {
+    type Color {
+        id: ID!
+        title: String!
+        text: String!
+        img: String!
+      }
+    
+      type Query {
+        getColors: [Color]
+        getColorsByTitle(title: String!): Color
+      }
+`;
 
-storiesOf('Flex', module).add('with test value', () => <Flex />);
+storiesOf('Flex', module)
+  .addDecorator(
+    apolloStorybookDecorator({
+      typeDefs,
+    })
+  )
+  .add('with test value', () => <Flex />);
--- client/src/views/HomePage.js
+++ client/src/views/HomePage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import Section from '../components/Section';
-import { BottomSection } from '../assets/Global.style.js';
 function HomePage() {
   return (
     <>
