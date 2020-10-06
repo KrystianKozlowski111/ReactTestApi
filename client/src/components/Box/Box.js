6b5dafb21d25c16cf4b6a210da9c7006e58e7472
KrystianKozlowski <krystiankozlowski111@gmail.com>
2020-10-06 15:25:32 +0200

    form

--- client/src/components/Box/Box.js
+++ client/src/components/Box/Box.js
@@ -1,19 +1,27 @@
 import React, { useState } from 'react';
 import { Wrapper, Edit, Image, Title, Text, Img } from './Box.style';
-
+import { useForm } from 'react-hook-form';
 const Box = (props) => {
   const { title, text, img, loading, error } = props;
-  const [editedTitle, setTitle] = useState(title);
+
+  const { register, handleSubmit, watch, errors } = useForm();
+  const onSubmit = (data) => {
+    let title1 = data.title;
+    console.log(title1);
+  };
+
   return (
     <Wrapper>
-      <Edit
-        value={editedTitle}
-        onInput={(e) => setTitle(e.target.value)}
-      ></Edit>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input name="title" defaultValue={title} ref={register} />
+
+        <input type="submit" />
+      </form>
+
       <Image>
         <Img src={img} />
       </Image>
-      <Title>{editedTitle}</Title>
+      <Title>{title}</Title>
       <Text>{text}</Text>
     </Wrapper>
   );
