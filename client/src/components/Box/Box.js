b6d99158ac6e353b5946daadc6104879aa696279
KrystianKozlowski <krystiankozlowski111@gmail.com>
2020-10-06 16:48:02 +0200

    Update Box.js

--- client/src/components/Box/Box.js
+++ client/src/components/Box/Box.js
@@ -3,28 +3,43 @@ import { Wrapper, Edit, Image, Title, Text, Img } from './Box.style';
 import { useForm } from 'react-hook-form';
 const Box = (props) => {
   const { title, text, img, loading, error } = props;
-
-  const { register, handleSubmit, watch, errors } = useForm();
+  const [editMode, setEditMode] = useState(false);
+  const { register, handleSubmit, watch, errors } = useForm({
+    defaultValues: {
+      title,
+      text,
+    },
+  });
   const onSubmit = (data) => {
     let editedTitle = data.title;
     console.log(editedTitle);
   };
-
   return (
     <Wrapper>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input name="title" defaultValue={title} ref={register} />
-
-        <input type="submit" />
+        <Image>
+          <Img src={img} />
+        </Image>
+        {editMode ? (
+          <input name="title" name={'title'} ref={register} />
+        ) : (
+          <Title>{title}</Title>
+        )}
+        {editMode ? (
+          <input name="title" name={'text'} ref={register} />
+        ) : (
+          <Text>{text}</Text>
+        )}
+        {editMode && <input type="submit" />}
       </form>
-
-      <Image>
-        <Img src={img} />
-      </Image>
-      <Title>{title}</Title>
-      <Text>{text}</Text>
+      <button
+        onClick={() => {
+          setEditMode(!editMode);
+        }}
+      >
+        editMode {editMode ? 'true' : 'false'}
+      </button>
     </Wrapper>
   );
 };
-
 export default Box;
