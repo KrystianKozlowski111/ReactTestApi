bf3f042f2a97372f71e4af6e1c838d98568723e8
KrystianKozlowski <krystiankozlowski111@gmail.com>
2020-10-07 10:51:16 +0200

    Delete button

--- client/src/components/Box/Box.js
+++ client/src/components/Box/Box.js
@@ -4,6 +4,7 @@ import { useForm } from 'react-hook-form';
 const Box = (props) => {
   const { title, text, img, loading, error } = props;
   const [editMode, setEditMode] = useState(false);
+
   const { register, handleSubmit, watch, errors } = useForm({
     defaultValues: {
       title,
@@ -15,34 +16,46 @@ const Box = (props) => {
     setText(data.text);
     setEditMode(!editMode);
   };
+  const [deleted, setDelete] = useState(false);
   const [editedTitle, setTitle] = useState(title);
   const [editedText, setText] = useState(text);
   return (
-    <Wrapper>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Image>
-          <Img src={img} />
-        </Image>
-        {editMode ? (
-          <input name="title" name={'title'} ref={register} />
-        ) : (
-          <Title>{editedTitle}</Title>
-        )}
-        {editMode ? (
-          <input name="title" name={'text'} ref={register} />
-        ) : (
-          <Text>{editedText}</Text>
-        )}
-        {editMode && <input type="submit" />}
-      </form>
-      <button
-        onClick={() => {
-          setEditMode(!editMode);
-        }}
-      >
-        editMode {editMode ? 'true' : 'false'}
-      </button>
-    </Wrapper>
+    <>
+      {deleted != true ? (
+        <Wrapper>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Image>
+              <Img src={img} />
+            </Image>
+            {editMode ? (
+              <input name="title" name={'title'} ref={register} />
+            ) : (
+              <Title>{editedTitle}</Title>
+            )}
+            {editMode ? (
+              <input name="title" name={'text'} ref={register} />
+            ) : (
+              <Text>{editedText}</Text>
+            )}
+            {editMode && <input type="submit" />}
+          </form>
+          <button
+            onClick={() => {
+              setEditMode(!editMode);
+            }}
+          >
+            Edit
+          </button>
+          <button
+            onClick={() => {
+              setDelete(!deleted);
+            }}
+          >
+            Delete
+          </button>
+        </Wrapper>
+      ) : null}
+    </>
   );
 };
 export default Box;
