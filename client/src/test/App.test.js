d960e93396aed965cbf67dfeeedb99e7e7fef458
KrystianKozlowski <krystiankozlowski111@gmail.com>
2020-10-28 11:47:09 +0100

    tests

--- client/src/test/App.test.js
+++ client/src/test/App.test.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Section from '../views/HomePage.js';
+import Section from '../components/Section';
+
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { ApolloProvider } from '@apollo/client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000',
+});
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
 
 it('renders Section without crashing', () => {
-  shallow(<Section />);
+  shallow(
+    <ApolloProvider client={client}>
+      <Section />
+    </ApolloProvider>
+  );
 });
-it('includes Title', () => {
-  const section = shallow(<Section />);
+it('includes Flex', () => {
+  const section = shallow(
+    <ApolloProvider client={client}>
+      <Section />
+    </ApolloProvider>
+  );
   expect(section.containsMatchingElement(<Flex />)).toEqual(true);
 });
